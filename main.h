7354096039699f2d7398820f1cc28d28637e9eef
pether20 <pether.te.z@hotmail.com>
2021-11-10 17:04:15 +0000

    Add new line 29 and line 30

--- main.h
+++ main.h
@@ -26,5 +26,7 @@ int print_string(va_list s);
 int print_char(va_list c);
 int (*search_character(const char *nxchr, int num))(va_list);
 int print_int(va_list i);
+int print_bin(va_list b);
+char *binconvert(int val, int base);
 #endif
 
